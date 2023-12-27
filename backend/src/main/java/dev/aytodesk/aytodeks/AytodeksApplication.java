6ce6cce20b4912b360e52029c7e5eb9b3e730eb4
Borja <elbarmandev@gmail.com>
2023-12-27 12:22:13 +0100

    first commit

---
+++ backend/src/main/java/dev/aytodesk/aytodeks/AytodeksApplication.java
@@ -0,0 +1,13 @@
+package dev.aytodesk.aytodeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AytodeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AytodeksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/aytodesk/aytodeks/AytodeksApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aytodesk.aytodeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AytodeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
